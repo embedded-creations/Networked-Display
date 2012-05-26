7a0af91be164fdbdb249f3b66e14ce1e1c672bab
Louis <louis@embedded-creations.com>
2012-05-26 16:59:25 -0700

    added support for libusb-win32, works

--- libusbtest/commandline/main.c
+++ libusbtest/commandline/main.c
@@ -21,9 +21,13 @@
 #include <sys/types.h>
 #include <errno.h>
 
-#include <libusb.h>
+//#define USE_LIBUSB_1_0
 
-static libusb_context *ctx = NULL;
+#ifdef USE_LIBUSB_1_0
+#include <libusb.h>
+#else
+#include <usb.h>
+#endif
 
 /* USB identifiers */
 #define USBDEVICE_SHARED_VID   0x16C0  /* VOTI */
@@ -37,6 +41,9 @@ static libusb_context *ctx = NULL;
 char progname[] = "libusbtest";
 char verbose = 2;
 
+
+
+#ifdef USE_LIBUSB_1_0
 static int libusb_to_errno(int result)
 {
     switch (result) {
@@ -74,8 +81,10 @@ static int libusb_to_errno(int result)
         return ERANGE;
     }
 }
+#endif
 
 
+#ifdef USE_LIBUSB_1_0
 static int usbOpenDevice(libusb_device_handle **device, int vendor,
              char *vendorName, int product, char *productName)
 {
@@ -87,11 +96,11 @@ static int usbOpenDevice(libusb_device_handle **device, int vendor,
 
     if(!didUsbInit){
         didUsbInit = 1;
-        libusb_init(&ctx);
+        libusb_init(NULL);
     }
 
     libusb_device **dev_list;
-    int dev_list_len = libusb_get_device_list(ctx, &dev_list);
+    int dev_list_len = libusb_get_device_list(NULL, &dev_list);
 
     for (j=0; j<dev_list_len; ++j) {
         libusb_device *dev = dev_list[j];
@@ -153,15 +162,106 @@ static int usbOpenDevice(libusb_device_handle **device, int vendor,
     }
     return errorCode;
 }
+#else
+static int usbOpenDevice(usb_dev_handle **device, int vendor,
+			 char *vendorName, int product, char *productName)
+{
+struct usb_bus       *bus;
+struct usb_device    *dev;
+usb_dev_handle       *handle = NULL;
+int                  errorCode = USB_ERROR_NOTFOUND;
+static int           didUsbInit = 0;
+
+    if(!didUsbInit){
+        didUsbInit = 1;
+        usb_init();
+    }
+    usb_find_busses();
+    usb_find_devices();
+    for(bus=usb_get_busses(); bus; bus=bus->next){
+        for(dev=bus->devices; dev; dev=dev->next){
+            if(dev->descriptor.idVendor == vendor &&
+	       dev->descriptor.idProduct == product){
+                char    string[256];
+                int     len;
+		/* we need to open the device in order to query strings */
+                handle = usb_open(dev);
+                if(!handle){
+                    errorCode = USB_ERROR_ACCESS;
+                    fprintf(stderr,
+			    "%s: Warning: cannot open USB device: %s\n",
+			    progname, usb_strerror());
+                    continue;
+                }
+                if(vendorName == NULL && productName == NULL){
+		    /* name does not matter */
+                    break;
+                }
+                /* now check whether the names match: */
+                len = usb_get_string_simple(handle, dev->descriptor.iManufacturer,
+					    string, sizeof(string));
+                if(len < 0){
+                    errorCode = USB_ERROR_IO;
+                    fprintf(stderr,
+			    "%s: Warning: cannot query manufacturer for device: %s\n",
+			    progname, usb_strerror());
+                }else{
+                    errorCode = USB_ERROR_NOTFOUND;
+		    if (verbose > 1)
+		        fprintf(stderr,
+				"%s: seen device from vendor ->%s<-\n",
+				progname, string);
+                    if(strcmp(string, vendorName) == 0){
+                        len = usb_get_string_simple(handle, dev->descriptor.iProduct,
+						    string, sizeof(string));
+                        if(len < 0){
+                            errorCode = USB_ERROR_IO;
+                            fprintf(stderr,
+				    "%s: Warning: cannot query product for device: %s\n",
+				    progname, usb_strerror());
+                        }else{
+                            errorCode = USB_ERROR_NOTFOUND;
+			    if (verbose > 1)
+			        fprintf(stderr,
+					"%s: seen product ->%s<-\n",
+					progname, string);
+                            if(strcmp(string, productName) == 0)
+                                break;
+                        }
+                    }
+                }
+                usb_close(handle);
+                handle = NULL;
+            }
+        }
+        if(handle)
+            break;
+    }
+    if(handle != NULL){
+        errorCode = 0;
+        *device = handle;
+    }
+    return errorCode;
+}
+#endif
+
 
 #define CUSTOM_RQ_TEST_READ     3
 #define CUSTOM_RQ_TEST_WRITE    4
 
-static int usbdevice_transmit(libusb_device_handle *device,
-               unsigned char receive, unsigned char functionid,
-               unsigned char send[4], unsigned char * buffer, int buffersize)
+
+#ifdef USE_LIBUSB_1_0
+    static int usbdevice_transmit(libusb_device_handle *device,
+                   unsigned char receive, unsigned char functionid,
+                   unsigned char send[4], unsigned char * buffer, int buffersize)
+#else
+    static int usbdevice_transmit(usb_dev_handle *device,
+                   unsigned char receive, unsigned char functionid,
+                   unsigned char send[4], unsigned char * buffer, int buffersize)
+#endif
 {
   int nbytes;
+#ifdef USE_LIBUSB_1_0
   nbytes = libusb_control_transfer(device,
                    (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE | (receive << 7)) & 0xff,
                    functionid & 0xff,
@@ -174,11 +274,29 @@ static int usbdevice_transmit(libusb_device_handle *device,
     fprintf(stderr, "%s: error: usbasp_transmit: %s\n", progname, strerror(libusb_to_errno(nbytes)));
     return -1;
   }
+#else
+  nbytes = usb_control_msg(device,
+               USB_TYPE_VENDOR | USB_RECIP_DEVICE | (receive << 7),
+               functionid,
+               (send[1] << 8) | send[0],
+               (send[3] << 8) | send[2],
+               (char *)buffer, buffersize,
+               5000);
+  if(nbytes < 0){
+    fprintf(stderr, "%s: error: usbasp_transmit: %s\n", progname, usb_strerror());
+    return -1;
+  }
+#endif
+
   return nbytes;
 }
 
 
+#ifdef USE_LIBUSB_1_0
 static int testWrite(libusb_device_handle *device)
+#else
+static int testWrite(usb_dev_handle *device)
+#endif
 {
   unsigned char temp[4];
   unsigned char res[64];
@@ -205,7 +323,11 @@ static int testWrite(libusb_device_handle *device)
   return 0;
 }
 
+#ifdef USE_LIBUSB_1_0
 static int usbasp_initialize(libusb_device_handle *device)
+#else
+static int usbasp_initialize(usb_dev_handle *device)
+#endif
 {
   unsigned char temp[4];
   unsigned char res[230];
@@ -233,9 +355,18 @@ static int usbasp_initialize(libusb_device_handle *device)
 
 
 /* Interface - prog. */
+#ifdef USE_LIBUSB_1_0
 static int usbdevice_open (libusb_device_handle **device)
+#else
+static int usbdevice_open (usb_dev_handle **device)
+#endif
 {
-    libusb_init(&ctx);
+#ifdef USE_LIBUSB_1_0
+    libusb_init(NULL);
+#else
+  usb_init();
+#endif
+
 
     if (usbOpenDevice(device, USBDEVICE_SHARED_VID, "Embedded Creations",
             USBDEVICE_SHARED_PID, "libusbtest") != 0)
@@ -252,6 +383,7 @@ static int usbdevice_open (libusb_device_handle **device)
 }
 
 
+#if 0
 static void print_devs(libusb_device **devs)
 {
 	libusb_device *dev;
@@ -270,13 +402,19 @@ static void print_devs(libusb_device **devs)
 			libusb_get_bus_number(dev), libusb_get_device_address(dev));
 	}
 }
-
+#endif
 
 
 int main(void)
 {
-	libusb_device **devs;
-	libusb_device_handle *usbhandle;
+#ifdef USE_LIBUSB_1_0
+    libusb_device_handle *usbhandle;
+#else
+    usb_dev_handle *usbhandle;
+#endif
+
+#if 0
+    libusb_device **devs;
 	int r;
 	ssize_t cnt;
 
@@ -289,8 +427,11 @@ int main(void)
 		return (int) cnt;
 
 	print_devs(devs);
+
 	libusb_free_device_list(devs, 1);
 
+    libusb_exit(NULL);
+#endif
 
 	if(!usbdevice_open(&usbhandle))
 	{
@@ -298,7 +439,7 @@ int main(void)
         usbasp_initialize(usbhandle);
 	}
 
-	libusb_exit(NULL);
+
 	return 0;
 }
 
