63ee8dfca536853a5fde9f89fb3dc20bc695b2c3
Louis <louis@embedded-creations.com>
2012-05-25 20:47:49 -0700

    copied code over from avrdude: able to find USBasp programmer (test device)

--- libusbtest/main.c
+++ libusbtest/main.c
@@ -19,8 +19,161 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <errno.h>
 
 #include <libusb.h>
+
+static libusb_context *ctx = NULL;
+
+/* USB identifiers */
+#define USBASP_SHARED_VID   0x16C0  /* VOTI */
+#define USBASP_SHARED_PID   0x05DC  /* Obdev's free shared PID */
+
+/* USB error identifiers */
+#define USB_ERROR_NOTFOUND  1
+#define USB_ERROR_ACCESS    2
+#define USB_ERROR_IO        3
+
+char progname[] = "avrdudetest";
+char verbose = 2;
+
+static int libusb_to_errno(int result)
+{
+    switch (result) {
+    case LIBUSB_SUCCESS:
+        return 0;
+    case LIBUSB_ERROR_IO:
+        return EIO;
+    case LIBUSB_ERROR_INVALID_PARAM:
+        return EINVAL;
+    case LIBUSB_ERROR_ACCESS:
+        return EACCES;
+    case LIBUSB_ERROR_NO_DEVICE:
+        return ENXIO;
+    case LIBUSB_ERROR_NOT_FOUND:
+        return ENOENT;
+    case LIBUSB_ERROR_BUSY:
+        return EBUSY;
+#ifdef ETIMEDOUT
+    case LIBUSB_ERROR_TIMEOUT:
+        return ETIMEDOUT;
+#endif
+#ifdef EOVERFLOW
+    case LIBUSB_ERROR_OVERFLOW:
+        return EOVERFLOW;
+#endif
+    case LIBUSB_ERROR_PIPE:
+        return EPIPE;
+    case LIBUSB_ERROR_INTERRUPTED:
+        return EINTR;
+    case LIBUSB_ERROR_NO_MEM:
+        return ENOMEM;
+    case LIBUSB_ERROR_NOT_SUPPORTED:
+        return ENOSYS;
+    default:
+        return ERANGE;
+    }
+}
+
+
+static int usbOpenDevice(libusb_device_handle **device, int vendor,
+             char *vendorName, int product, char *productName)
+{
+    libusb_device_handle *handle = NULL;
+    int                  errorCode = USB_ERROR_NOTFOUND;
+    static int           didUsbInit = 0;
+    int j;
+    int r;
+
+    if(!didUsbInit){
+        didUsbInit = 1;
+        libusb_init(&ctx);
+    }
+
+    libusb_device **dev_list;
+    int dev_list_len = libusb_get_device_list(ctx, &dev_list);
+
+    for (j=0; j<dev_list_len; ++j) {
+        libusb_device *dev = dev_list[j];
+        struct libusb_device_descriptor descriptor;
+    libusb_get_device_descriptor(dev, &descriptor);
+    if (descriptor.idVendor == vendor && descriptor.idProduct == product) {
+            char    string[256];
+        /* we need to open the device in order to query strings */
+            r = libusb_open(dev, &handle);
+            if (!handle) {
+                 errorCode = USB_ERROR_ACCESS;
+                 fprintf(stderr,
+                "%s: Warning: cannot open USB device: %d %s\n",
+                progname, r, strerror(libusb_to_errno(r)));
+                    continue;
+                }
+                if (vendorName == NULL && productName == NULL) {
+            /* name does not matter */
+                    break;
+                }
+                /* now check whether the names match: */
+        r = libusb_get_string_descriptor_ascii(handle, descriptor.iManufacturer & 0xff, (unsigned char *)string, sizeof(string));
+                if (r < 0) {
+                    errorCode = USB_ERROR_IO;
+                    fprintf(stderr,
+                "%s: Warning: cannot query manufacturer for device: %s\n",
+                progname, strerror(libusb_to_errno(r)));
+                } else {
+                    errorCode = USB_ERROR_NOTFOUND;
+            if (verbose > 1)
+                fprintf(stderr,
+                "%s: seen device from vendor ->%s<-\n",
+                progname, string);
+                    if (strcmp(string, vendorName) == 0){
+            r = libusb_get_string_descriptor_ascii(handle, descriptor.iProduct & 0xff, (unsigned char *)string, sizeof(string));
+                        if (r < 0) {
+                            errorCode = USB_ERROR_IO;
+                            fprintf(stderr,
+                    "%s: Warning: cannot query product for device: %s\n",
+                    progname, strerror(libusb_to_errno(r)));
+                        } else {
+                            errorCode = USB_ERROR_NOTFOUND;
+                if (verbose > 1)
+                    fprintf(stderr,
+                    "%s: seen product ->%s<-\n",
+                    progname, string);
+                            if(strcmp(string, productName) == 0)
+                                break;
+                        }
+                    }
+                }
+                libusb_close(handle);
+                handle = NULL;
+            }
+    }
+    if (handle != NULL){
+        errorCode = 0;
+        *device = handle;
+    }
+    return errorCode;
+}
+
+
+/* Interface - prog. */
+static int usbasp_open(libusb_device_handle **device)
+{
+  libusb_init(&ctx);
+
+if (usbOpenDevice(device, USBASP_SHARED_VID, "www.fischl.de",
+            USBASP_SHARED_PID, "USBasp") != 0) {
+
+      /* no USBasp found */
+      fprintf(stderr,
+          "error: could not find USB device "
+          "\"USBasp\" with vid=0x%x pid=0x%x\n",
+          USBASP_SHARED_VID, USBASP_SHARED_PID);
+      return -1;
+    }
+
+  return 0;
+}
+
 
 static void print_devs(libusb_device **devs)
 {
@@ -41,9 +194,12 @@ static void print_devs(libusb_device **devs)
 	}
 }
 
+
+
 int main(void)
 {
 	libusb_device **devs;
+	libusb_device_handle *usbhandle;
 	int r;
 	ssize_t cnt;
 
@@ -57,6 +213,9 @@ int main(void)
 
 	print_devs(devs);
 	libusb_free_device_list(devs, 1);
+
+
+	usbasp_open(&usbhandle);
 
 	libusb_exit(NULL);
 	return 0;
